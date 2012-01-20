4a6c77b06a0eefd96afe7d8c426a6ec4243d51a4
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-01-20 18:19:29 +0100

    added validations when creating new students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ActiveRecord::Base
+  validate :validate_email
+  validates_presence_of :name
+
+  def validate_email
+    if not email.match(/@/)
+      errors[:email].push("invalid")
+    end
+  end
+
 end
