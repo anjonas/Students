adc1356c8e23478a00b88e658eadea4518481d08
Jonas Ängeslevä <anjonas@student.chalmers.se>
2012-01-20 16:36:56 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Students::Application.config.secret_token = 'e8fdf989cc566e336221c7295b93b5a3fe8332e84c95dbce31952b8bd57d23295dd4627c27c4373db1386393d7ab89e5d2afaa4e51ceac0b470576ba0b68f3e2'
